535b30d56270d918fa46662d67e6dbdee194fa22
livm <livm@student.chalmers.se>
2020-12-21 19:40:06 +0100

    Text added to display import in dom

--- src/components/RainbowContainer.js
+++ src/components/RainbowContainer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const RainbowContainer = () => {
-  return <div className={'rainbow-container'}></div>
+  return (
+    <div className={'rainbow-container container'}>
+      <h1>Rainbow container</h1>
+    </div>
+  )
 }
 
 export default RainbowContainer
